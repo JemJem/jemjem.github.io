1fc0702f1a8983701c8e9a715def949883e186be
jemben <jbenazra@palo-it.com>
2018-04-16 10:35:06 +1000

    Aligning Menu headers
    Harmonising menu header accross workshop page and home page

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -1002,4 +1002,4 @@
         }
     }
 })(jQuery);
-!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.me">Mobirise</a> Mobirise v4.7.1';document.body.insertBefore(a,document.body.childNodes[0])}}();
+!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.me">Mobirise</a> Mobirise v4.7.0';document.body.insertBefore(a,document.body.childNodes[0])}}();
