7c932ac1ef75a5bc4602c8d5deb224533232ab97
jemben <jbenazra@palo-it.com>
2018-10-29 15:22:06 +1100

    workshop outlook and sponsors

--- assets/theme/js/script.js
+++ assets/theme/js/script.js
@@ -1002,4 +1002,4 @@
         }
     }
 })(jQuery);
-!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobiri.se">Mobirise</a> Mobirise v4.7.6';document.body.insertBefore(a,document.body.childNodes[0])}}();
+!function(){try{document.getElementsByClassName("engine")[0].getElementsByTagName("a")[0].removeAttribute("rel")}catch(b){}if(!document.getElementById("top-1")){var a=document.createElement("section");a.id="top-1";a.className="engine";a.innerHTML='<a href="https://mobirise.ws">Mobirise Website Builder</a> v4.8.6';document.body.insertBefore(a,document.body.childNodes[0])}}();
